51e7591a50cbdc85df4b40dfbe1e2ba8b83dd405
sarahmsoh <sarahmuriithi523@gmail.com>
2024-09-23 20:03:40 +0300

    new

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10);
+let num3 = 9
+let num4 = 5
+const mod = num3 % num4
+const max = Math.max(3, 6, 20, 9)
